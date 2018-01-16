72a2425dde7e476cba30b0327f9daebf1b45f271
TimothyRoberts <timdwroberts@gmail.com>
2018-01-16 13:35:51 +0000

    Uniform Distribution

--- Uniform Distribution/sketch.js
+++ Uniform Distribution/sketch.js
@@ -1,28 +1,31 @@
-var walker;
+var walker; 
 
 function setup() {
-  createCanvas(320,640);
+  createCanvas(320,640); 
   background(127);
   walker = new Walker();
 }
 
+//LOOPS AND EXECUTES WALKER FUNCTIONS
 function draw() {
   walker.render();
   walker.step();
 }
 
+//WALKER CLASS
 function Walker() {
   this.x = width/2;
   this.y = height/2;
 
+  //DRAWS POINT ON CANVAS
   this.render = function(){
     stroke(0);
     point(this.x,this.y);
   }
 
-
+  //RANDOMLY CHANGES POINT X/Y COORDINATES
   this.step = function(){
-    var choice = floor(random(4));
+    var choice = floor(random(4)); //floor() changes the random float into its rounded down integer
     if(choice === 0){
       this.x++;
     } 
@@ -35,5 +38,11 @@ function Walker() {
     else {
       this.y--;
     }
+
+    //Restricts point's x and y to the canvas
+    this.x = constrain(this.x, 0, width);
+    this.y = constrain(this.y, 0, height);
+
   }
+
 }

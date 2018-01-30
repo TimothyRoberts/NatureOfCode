66ad63bd3012534ea9896af956bd9c6e6611bd21
TimothyRoberts <timdwroberts@gmail.com>
2018-01-30 10:18:10 +0000

    Added Comments

--- 01_Randomness_Probability_PerlinNoise/07_Two_Dimensional_Perlin_Noise/sketch.js
+++ 01_Randomness_Probability_PerlinNoise/07_Two_Dimensional_Perlin_Noise/sketch.js
@@ -6,6 +6,7 @@ function setup() {
   noOfWalkers = 10;
   walkers = [];
 
+  //Loops 10 times and creates 10 walker objects
   for(i = 0; i < noOfWalkers; i++){
     walkers[i] = new Walker();
   }
@@ -13,10 +14,11 @@ function setup() {
 }
 
 function draw() { 
-  //Covering canvas with light opacity rectangle
-  //each frame to create trail effect
   fill(0,15);
-  rect(0, 0, width, height); 
+  rect(0, 0, width, height);
+
+  //loops 10 times - steps and renders each
+  //walker object in the array
   for(i = 0; i < noOfWalkers; i++) {
     walkers[i].step();
     walkers[i].render();
@@ -24,20 +26,31 @@ function draw() {
 }
 
 function Walker() {
+
+  //position set to center of canvas
+  //noff set to random x, y point between 1000, 1000
   this.position = createVector(width/2, height/2);
   this.noff = createVector(random(1000), random(1000));
+
+  this.step = function() {
+    //x and y position now set to smoothly move randomly along 
+    //(and 200 pixels off) canvas
+    this.position.x = map(noise(this.noff.x), 0, 1, 0 -200, width + 200);
+    this.position.y = map(noise(this.noff.y), 0, 1, 0 -200, height + 200);
+    //this controls how smooth the random movement will be
+    this.noff.add(0.005, 0.005);
+  }
 
   this.render = function() {
     noStroke();
     fill(255, 45, 0, 50);
+
+    //ellipse drawn at established x and y points
+    //size is then mapped between 20 and 200 using perlin noise
     ellipse(this.position.x, this.position.y, map(noise(this.noff.x), 0, 1, 20, 200), 
       map(noise(this.noff.x), 0, 1, 20, 200));
-  }
 
-  this.step = function() {
-    this.position.x = map(noise(this.noff.x), 0, 1, 0 -200, width + 200);
-    this.position.y = map(noise(this.noff.y), 0, 1, 0 -200, height + 200);
-    this.noff.add(0.005, 0.005);
   }
 
 }
+
